4d114c095b9bb39518a9846b790029f2e79e5ddf
jonas <79740648+nas93@users.noreply.github.com>
2021-12-04 08:02:47 +0100

    Update text.js

--- assets/js/text.js
+++ assets/js/text.js
@@ -4,7 +4,7 @@ function typingEffect() {
     var typewriter = new Typewriter(app, {
         loop: true
     });
-    var array = ['best frontend dev ','car enthusiast ','highly skilled noob ', 'average bmw fan ', 'insane rust beamer ', 'new discord bot dev']
+    var array = [' best frontend dev ',' car enthusiast ',' highly skilled noob ', ' bmw fan ', ' insane rust beamer ', ' discord bot dev']
     
     for(i = 0; i < array.length; i++)
     {
